8866bdc8b8faf91258fec46c7180a17c591c4e92
rodolfo-riveros <1001420172@unajma.edu.pe>
2024-07-13 11:03:06 -0500

    chart.js

--- resources/views/admin/home/index.blade.php
+++ resources/views/admin/home/index.blade.php
@@ -30,5 +30,67 @@
 @stop
 
 @section('js')
-    {{-- <script> console.log("Hi, I'm using the Laravel-AdminLTE package!"); </script> --}}
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script>
+        document.addEventListener('DOMContentLoaded', function(){
+            // Datos y opciones para el gráfico de área
+            var ctxRevenue = document.getElementById('revenue-chart-canvas').getContext('2d');
+            var revenueChart = new Chart(ctxRevenue, {
+                type: 'line',
+                data: {
+                    labels: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+                    datasets: [{
+                        label: 'Revenue',
+                        data: [12, 74, 24, 10, 62, 58, 89, 100, 78, 47, 45, 25],
+                        backgroundColor: 'rgba(60,141,188,0.9)',
+                        borderColor: 'rgba(60,141,188,0.8)',
+                        pointRadius: false,
+                        pointColor: '#3b8bba',
+                        pointStrokeColor: 'rgba(60,141,188,1)',
+                        pointHighlightFill: '#fff',
+                        pointHighlightStroke: 'rgba(60,141,188,1)',
+                        fill: true // Para que el área debajo de la línea esté rellena
+                    }]
+                },
+                options: {
+                    maintainAspectRatio: false,
+                    responsive: true,
+                    legend: {
+                        display: false
+                    },
+                    scales: {
+                        xAxes: [{
+                            gridLines: {
+                                display: false,
+                            }
+                        }],
+                        yAxes: [{
+                            gridLines: {
+                                display: false,
+                            }
+                        }]
+                    }
+                }
+            });
+            // Datos y opciones para el gráfico de dona
+            var ctxSales = document.getElementById('sales-chart-canvas').getContext('2d');
+            var salesChart = new Chart(ctxSales, {
+                type: 'doughnut',
+                data: {
+                    labels: ['Consolas', 'Computadoras', 'Perifericos'],
+                    datasets: [{
+                        data: [30, 50, 20],
+                        backgroundColor: ['#f56954', '#00a65a', '#f39c12'],
+                    }]
+                },
+                options: {
+                    maintainAspectRatio: false,
+                    responsive: true,
+                    legend: {
+                        display: false
+                    }
+                }
+            });
+        });
+    </script>
 @stop
--- resources/views/livewire/admin/home-index.blade.php
+++ resources/views/livewire/admin/home-index.blade.php
@@ -7,10 +7,11 @@
                         <h3>{{ $users }}</h3>
                         <p>Usuarios</p>
                     </div>
-                <div class="icon">
-                    <i class="ion ion-bag"></i>
-                </div>
-                <a href="{{ route('admin.usuario.index') }}" class="small-box-footer">Más información<i class="fas fa-arrow-circle-right"></i></a>
+                    <div class="icon">
+                        <i class="fa fa-user"></i>
+                    </div>
+                    <a href="{{ route('admin.usuario.index') }}" class="small-box-footer">Más información<i
+                            class="fas fa-arrow-circle-right"></i></a>
                 </div>
             </div>
             <div class="col-lg-3 col-6">
@@ -19,34 +20,67 @@
                         <h3>{{ $categorias }}</h3>
                         <p>CATEGORÍAS</p>
                     </div>
-                <div class="icon">
-                    <i class="ion ion-stats-bars"></i>
-                </div>
-                <a href="{{ route('admin.categoria.index') }}" class="small-box-footer">Más información<i class="fas fa-arrow-circle-right"></i></a>
+                    <div class="icon">
+                        <i class="fa fa-layer-group"></i>
+                    </div>
+                    <a href="{{ route('admin.categoria.index') }}" class="small-box-footer">Más información<i
+                            class="fas fa-arrow-circle-right"></i></a>
                 </div>
             </div>
             <div class="col-lg-3 col-6">
                 <div class="small-box bg-warning">
-                <div class="inner">
-                    <h3>{{ $productos }}</h3>
-                    <p>PRODUCTOS</p>
-                </div>
-                <div class="icon">
-                    <i class="ion ion-person-add"></i>
-                </div>
-                <a href="{{ route('admin.producto.index') }}" class="small-box-footer">Más información<i class="fas fa-arrow-circle-right"></i></a>
+                    <div class="inner">
+                        <h3>{{ $productos }}</h3>
+                        <p>PRODUCTOS</p>
+                    </div>
+                    <div class="icon">
+                        <i class="fa fa-gamepad"></i>
+                    </div>
+                    <a href="{{ route('admin.producto.index') }}" class="small-box-footer">Más información<i
+                            class="fas fa-arrow-circle-right"></i></a>
                 </div>
             </div>
             <div class="col-lg-3 col-6">
                 <div class="small-box bg-danger">
-                <div class="inner">
-                    <h3>{{ $pedidos }}</h3>
-                    <p>PEDIDOS</p>
+                    <div class="inner">
+                        <h3>{{ $pedidos }}</h3>
+                        <p>PEDIDOS</p>
+                    </div>
+                    <div class="icon">
+                        <i class="fa fa-truck"></i>
+                    </div>
+                    <a href="{{ route('admin.pedido.index') }}" class="small-box-footer">Más información<i
+                            class="fas fa-arrow-circle-right"></i></a>
                 </div>
-                <div class="icon">
-                    <i class="ion ion-pie-graph"></i>
+            </div>
+        </div>
+    </div>
+    <div class="p-2">
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">
+                    <i class="fas fa-chart-pie mr-1"></i>
+                    Sales
+                </h3>
+                <div class="card-tools">
+                    <ul class="nav nav-pills ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#revenue-chart" data-toggle="tab">Area</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#sales-chart" data-toggle="tab">Donut</a>
+                        </li>
+                    </ul>
                 </div>
-                <a href="{{ route('admin.pedido.index') }}" class="small-box-footer">Más información<i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+            <div class="card-body">
+                <div class="tab-content p-0">
+                    <div class="chart tab-pane active" id="revenue-chart" style="position: relative; height: 300px;">
+                        <canvas id="revenue-chart-canvas" height="w-full" style="height: 300px;"></canvas>
+                    </div>
+                    <div class="chart tab-pane" id="sales-chart" style="position: relative; height: 300px;">
+                        <canvas id="sales-chart-canvas" height="w-full" style="height: 300px;"></canvas>
+                    </div>
                 </div>
             </div>
         </div>
